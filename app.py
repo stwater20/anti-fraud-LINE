0d8fd731d34ff3bbc679bc9a0dff78495358cec0
ryan.chen <54sakuramiku@gmail.com>
2022-12-17 13:15:16 +0800

    init

--- app.py
+++ app.py
@@ -1,29 +1,48 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-app = Flask(__name__)
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory , jsonify
+import requests
+import urllib.request
+import pandas as pd
+import json
 
+def download_data():
+    try:
+        res = urllib.request.urlretrieve("https://data.moi.gov.tw/MoiOD/System/DownloadFile.aspx?DATA=7F6BE616-8CE6-449E-8620-5F627C22AA0D", "dataset.csv")
+        return res
+    except Exception as e:
+        print(e)
+        return False
+
+def get_data():
+    data = pd.read_csv('dataset.csv')
+    return data
+    
+df = get_data()
+dataset = df.values.tolist()
+jsonStr = json.dumps(dataset)
+print(type(jsonStr))
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    print('Request for index page received')
+    return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/data')
+def data():
+    return jsonify(dataset)
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
+@app.route('/search', methods=['POST'])
+def search():
+    name = request.form.get('LINE')
+    print(df[df["帳號"]==name].to_dict())
+    if name:
        print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
+       return jsonify(jsonStr)
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
    app.run()
